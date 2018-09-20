df282d2330bca82f4024dd2cd6812477243e3e2e
kmbruce1986 <kmbruce1986@gmail.com>
2018-09-20 10:03:42 +0100

    adds test files - not complete

---
+++ card_game.rb
@@ -0,0 +1,34 @@
+require_relative('card.rb')
+class CardGame
+
+
+  def checkforAce(card)
+    if card.value = 1
+      return true
+    else
+      return false
+    end
+  end
+
+# syntax errors - dif should say def to define the method and there should be a comma between the two parameters
+  dif highest_card(card1 card2)
+  if card1.value > card2.value
+    # logic error - card does not have a name
+    return card.name
+  else
+    card2
+  end
+end
+# syntax error - extra end
+end
+
+def self.cards_total(cards)
+  # logic error - total is not defined
+  total
+  for card in cards
+    total += card.value
+    return "You have a total of" + total
+  end
+end
+
+# syntax error - missing end to close the class
---
+++ specs/card_game_spec.rb
@@ -0,0 +1,37 @@
+require("minitest/autorun")
+require("minitest/rg")
+
+require_relative("../card_game.rb")
+require_relative("../card.rb")
+
+class TestCardGame < MiniTest::Test
+
+  def setup()
+    @card1 = Card.new("Hearts", 1)
+    @card2 = Card.new("Spades", 10)
+    @card3 = Card.new("Diamonds", 4)
+    @card_array = [@card1, @card2, @card3]
+    @card_game = CardGame.new()
+  end
+
+  def test_check_for_ace
+    result = @card_game.checkforAce(@card1)
+    assert_equal(true, result)
+  end
+
+  def test_check_for_ace
+    result = @card_game.checkforAce(@card2)
+    assert_equal(false, result)
+  end
+
+  def test_check_highest_card
+    result = @card_game.highest_card(@card1, @card2)
+    assert_equal(10, result)
+  end
+
+  def test_cards_total
+    result = @card_game.cards_total(@card_array)
+    assert_equal(15, result)
+  end
+
+end
